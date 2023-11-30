cc7cde6aa4271dd3db5ca189f89d0598c48757fa
devyanaby <devyanaby@gmail.com>
2023-12-01 06:58:41 +0700

    affh kelar

---
+++ lib/app/modules/views/alarm_page.dart
@@ -0,0 +1,122 @@
+import 'dart:async';
+import 'package:alarm/alarm.dart';
+import 'edit_alarm.dart';
+import 'ring.dart';
+import 'widgets/tile.dart';
+import 'package:flutter/material.dart';
+
+class ExampleAlarmHomeScreen extends StatefulWidget {
+  const ExampleAlarmHomeScreen({Key? key}) : super(key: key);
+  @override
+  State<ExampleAlarmHomeScreen> createState() => _ExampleAlarmHomeScreenState();
+}
+
+class _ExampleAlarmHomeScreenState extends State<ExampleAlarmHomeScreen> {
+  late List<AlarmSettings> alarms;
+  static StreamSubscription<AlarmSettings>? subscription;
+
+  @override
+  void initState() {
+    super.initState();
+    loadAlarms();
+    subscription ??= Alarm.ringStream.stream.listen(
+      (alarmSettings) => navigateToRingScreen(alarmSettings),
+    );
+  }
+
+  Future<void> navigateToRingScreen(AlarmSettings alarmSettings) async {
+    await Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) =>
+              ExampleAlarmRingScreen(alarmSettings: alarmSettings),
+        ));
+    loadAlarms();
+  }
+
+  void loadAlarms() {
+    setState(() {
+      alarms = Alarm.getAlarms();
+      alarms.sort((a, b) => a.dateTime.isBefore(b.dateTime) ? 0 : 1);
+    });
+  }
+
+  Future<void> navigateToAlarmScreen(AlarmSettings? settings) async {
+    final res = await showModalBottomSheet<bool?>(
+        context: context,
+        isScrollControlled: true,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        builder: (context) {
+          return FractionallySizedBox(
+            heightFactor: 0.75,
+            child: ExampleAlarmEditScreen(alarmSettings: settings),
+          );
+        });
+
+    if (res != null && res == true) loadAlarms();
+  }
+
+  @override
+  void dispose() {
+    subscription?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       backgroundColor: Color.fromARGB(255, 4, 80, 91),
+      appBar: AppBar(title: const Text('Alarm',  
+      style: TextStyle(
+            color: Colors.white, // Set text color to white
+            fontWeight: FontWeight.bold, // Set text to bold
+            ),
+          ), 
+          backgroundColor: Color.fromARGB(255, 4, 80, 91)),
+      body: SafeArea(
+        child: alarms.isNotEmpty
+            ? ListView.separated(
+                itemCount: alarms.length,
+                separatorBuilder: (context, index) => const Divider(height: 1),
+                itemBuilder: (context, index) {
+                  return ExampleAlarmTile(
+                    key: Key(alarms[index].id.toString()),
+                    title: TimeOfDay(
+                      hour: alarms[index].dateTime.hour,
+                      minute: alarms[index].dateTime.minute,
+                    ).format(context),
+                    onPressed: () => navigateToAlarmScreen(alarms[index]),
+                    onDismissed: () {
+                      Alarm.stop(alarms[index].id).then((_) => loadAlarms());
+                    },
+                  );
+                },
+              )
+                      : Center(
+                child: Text(
+                  "No alarms set",
+                  style: TextStyle(
+                    color: Colors.white, // Set text color to white
+                    fontSize: 16, // Set font size
+                  ),
+                ),
+              ),
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FloatingActionButton(
+              onPressed: () => navigateToAlarmScreen(null),
+              child: const Icon(Icons.alarm_add_rounded, size: 33),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
